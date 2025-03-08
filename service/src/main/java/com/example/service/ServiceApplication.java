973533b8016e270b19dea7b01174da88e86f65bd
Josh Long <josh@joshlong.com>
2025-03-08 13:56:31 -0800

    gotta support rabbitmq

--- service/src/main/java/com/example/service/ServiceApplication.java
+++ service/src/main/java/com/example/service/ServiceApplication.java
@@ -1,13 +1,34 @@
 package com.example.service;
 
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.amqp.dsl.Amqp;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.dsl.*;
+import org.springframework.messaging.MessageChannel;
 
 @SpringBootApplication
 public class ServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+
+    @Bean
+    IntegrationFlow outboundFlow(MessageChannel outboundMessageChannel,
+                                 AmqpTemplate amqpTemplate) {
+        return flow -> flow
+                .handle(Amqp
+                        .outboundAdapter(amqpTemplate)
+                        .exchangeName("messages")
+                );
+    }
+
+    @Bean
+    MessageChannelSpec<DirectChannelSpec, DirectChannel> outboundMessageChannel() {
+        return MessageChannels.direct();
+    }
 
 }
